87b4f066e61e4db665806d685df7d2a0a2c798a7
Marcello Brasileiro <victor.brasileiro6@hotmail.com>
2023-11-14 14:13:57 -0400

    utilizando outra animação para o titulo da pagina sobre mim

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-//  function ativaLetra(elemento) {
-//      const arrTexto = elemento.innerHTML.split(''); /*o split vai separar tudo o que
-//                                                       tiver no elemento*/
-//      elemento.innerHTML = ' '; /*vai ser vazio ou deixa separado*/
-//      arrTexto.forEach((letra, i)=>{
-//          setTimeout(()=>{
-//              elemento.innerHTML += letra;
-//          }, 75 * i);
-//      });
-//  }
+//    function ativaLetra(elemento) {
+//        const arrTexto = elemento.innerHTML.split(''); /*o split vai separar tudo o que
+//                                                         tiver no elemento*/
+//        elemento.innerHTML = ' '; /*vai ser vazio ou deixa separado*/
+//        arrTexto.forEach((letra, i)=>{
+//            setTimeout(()=>{
+//                elemento.innerHTML += letra;
+//            }, 100 * i);
+//        });
+//    }
 
-//  const titulo = document.querySelector('.nome-pessoal');
-//  ativaLetra(titulo);
+//    const titulo = document.querySelector('.digitando');
+//    ativaLetra(titulo)
 
-
+document.addEventListener('DOMContentLoaded', function () {
+    // Seu título
+    var title = document.getElementById('.digitando');
+  
+    // Função para reiniciar a animação
+    function restartAnimation() {
+      title.style.animation = 'none';
+      void title.offsetWidth; // Forçar uma reflow
+      title.style.animation = null;
+    }
+  
+    // Adiciona um ouvinte de evento para reiniciar a animação quando terminar
+    title.addEventListener('animationiteration', function () {
+      restartAnimation();
+    });
+  
+    // Inicializa a animação
+    restartAnimation();
+  });
+  
